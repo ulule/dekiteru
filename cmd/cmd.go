a447b7b2eb742adae748b192cd70dd48b49907f8
Gilles Fabio <gilles@gillesfabio.com>
2017-11-30 16:02:50 +0100

    Use constants for app name/version/desc.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,12 +8,18 @@ import (
 	"github.com/ulule/dekiteru/cmd/check"
 )
 
+const (
+	name        = "Dekiteru"
+	version     = "0.1.0"
+	description = "Check if a service is ready to use"
+)
+
 // Run runs the CLI application.
 func Run() error {
 	a := cli.NewApp()
-	a.Name = "Dekiteru"
-	a.Version = "0.1.0"
-	a.Usage = "Check if a service is ready to use"
+	a.Name = name
+	a.Version = version
+	a.Usage = description
 	a.Flags = []cli.Flag{}
 	a.Commands = []cli.Command{check.Command}
 	return a.Run(os.Args)
